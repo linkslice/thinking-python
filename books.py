a7b99972bbe53161f19ea0b800a28e7c78a69870
linkslice <sparctacus@gmail.com>
2012-07-22 00:31:59 -0700

    updated coments an' stuff

--- books.py
+++ books.py
@@ -2,10 +2,10 @@
 
 price = float(24.95)
 wholesaleDiscount = float(0.60)
-orderAmount = int(60)
+orderAmount = int(60) # wholesale price is 40% off, which means 60% remains
 shippingOne = int(3)
 shippingAdditional = float(0.75)
 
-print("Total price for %s books is: %s") % (orderAmount, (((price * wholesaleDiscount) + shippingOne) + ((price * wholesaleDiscount) + shippingAdditional) * (orderAmount - 1))) 
+print("Total price for %s books is: $%s") % (orderAmount, (((price * wholesaleDiscount) + shippingOne) + ((price * wholesaleDiscount) + shippingAdditional) * (orderAmount - 1))) 
 
 
